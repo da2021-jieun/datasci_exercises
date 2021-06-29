5c7ed12b9f5a2cdb87b5bd69fae92051c4831871
da2021-jieun <wxcolab@gmail.com>
2021-06-30 04:36:39 +0900

    Update .py

--- timeseries_walmart_kaggle.py
+++ timeseries_walmart_kaggle.py
@@ -97,11 +97,121 @@ plt.figure(figsize=(10,5))
 sns.lineplot(x=weekly_sales_2010.index,y=weekly_sales_2010.values)
 sns.lineplot(x=weekly_sales_2011.index,y=weekly_sales_2011.values)
 sns.lineplot(x=weekly_sales_2012.index,y=weekly_sales_2012.values)
-plt.grid()
 plt.xticks(np.arange(1,53))
 plt.legend(["2010","2011","2012"],loc="best",fontsize=11)
 plt.title("Average Weekly Sales, 2010-2012",fontsize=12)
 plt.ylabel("sales")
 plt.xlabel("week")
 plt.tight_layout()
+plt.grid()
 plt.show()
+
+### ==================================================
+### Change IsHoliday flag to true for Easter week
+train_detail.loc[(train_detail.Year==2010)&(train_detail.Week==13),"IsHoliday"]= True
+train_detail.loc[(train_detail.Year==2011)&(train_detail.Week==16),"IsHoliday"]= True
+train_detail.loc[(train_detail.Year==2012)&(train_detail.Week==14),"IsHoliday"]= True
+test_detail.loc[(test_detail.Year==2013)&(test_detail.Week==13),"IsHoliday"]= True
+
+### ==================================================
+### Sales median and mean values
+### - When data is significantly *skewed*—i.e., the distribution deviates from or distorts the symmetrical bell curve—then the median becomes far more representative of what’s ‘typical’ than the mean because it splits the data exactly in two, with 50% being above the median and 50% below.
+weekly_sales_mean= train_detail.Weekly_Sales.groupby(train_detail.Date).mean()
+weekly_sales_median= train_detail.Weekly_Sales.groupby(train_detail.Date).median()
+plt.figure(figsize=(10,5))
+sns.lineplot(x=weekly_sales_mean.index,y=weekly_sales_mean.values)
+sns.lineplot(x=weekly_sales_median.index,y=weekly_sales_median.values)
+plt.grid()
+plt.legend(["Mean","Median"])
+plt.title("Weekly Sales: Mean and Median")
+plt.ylabel("Sales")
+plt.xlabel("Date")
+plt.tight_layout()
+plt.show()
+
+### ==================================================
+### Average sales per store
+weekly_sales= train_detail.Weekly_Sales.groupby(train_detail.Store).mean()
+plt.figure(figsize=(10,5))
+sns.barplot(x=weekly_sales.index,y=weekly_sales.values,palette="winter") # spring, summer, autumn, winter,gnuplot2, copper,Blues_d,Spectral
+plt.title("Average Sales per Store")
+plt.ylabel("Sales")
+plt.xlabel("Store")
+plt.show()
+
+### ==================================================
+### Average sales per department
+weekly_sales= train_detail.Weekly_Sales.groupby(train_detail.Dept).mean()
+plt.figure(figsize=(18,5))
+sns.barplot(x=weekly_sales.index,y=weekly_sales.values,palette="copper_r")
+plt.title("Average Sales per Dept")
+plt.ylabel("Sales")
+plt.xlabel("Dept")
+plt.show()
+
+### ==================================================
+### Feature correlation
+### - Pearson correlation
+###     * 0: none
+###     * [0,.3]: weak
+###     * [.3,.7]: moderate
+###     * [.7,1]: strong
+sns.set_style("whitegrid")
+corr= round(train_detail.corr(),2)
+mask= np.triu(np.ones_like(corr,dtype=np.bool))
+fig,ax= plt.subplots(figsize=(12,8))
+cmap= sns.diverging_palette(240, 10,as_cmap=True)
+plt.title("Correlation Matrix")
+sns.heatmap(corr,mask=mask,cmap=cmap,vmax=.3,center=0,square=True,linewidths=.5,cbar_kws={"shrink":.5},annot=True)
+plt.show()
+
+### ==================================================
+### Drop insignificant columns
+### - MarkDown1,2,3,4,5 have very little correlation with sales
+### - Fuel_Price is also linearly dependent on Year
+train_detail.drop(columns=["MarkDown1","MarkDown2","MarkDown3","MarkDown4","MarkDown5","Fuel_Price"],inplace=True)
+test_detail.drop(columns=["MarkDown1","MarkDown2","MarkDown3","MarkDown4","MarkDown5","Fuel_Price"],inplace=True)
+
+## EDA
+### - For discrete variables: boxplot, stripplot
+### - For continuous variables: boxcox, scatterplot
+from matplotlib.gridspec import GridSpec
+def plot_discrete(series):
+    fig= plt.figure(figsize=(12,5))
+    gs= GridSpec(1, 2) #row 1,col 2
+    # boxplot
+    sns.boxplot(y=train_detail.Weekly_Sales,x=train_detail[series],ax=fig.add_subplot(gs[0,0]))
+    plt.ylabel("Sales")
+    plt.xlabel(series)
+    # stripplot
+    sns.stripplot(y=train_detail.Weekly_Sales,x=train_detail[series],ax=fig.add_subplot(gs[0,1]))
+    plt.ylabel("Sales")
+    plt.xlabel(series)
+    # fig.show()
+    plt.show()
+
+from scipy import stats
+from scipy.special import boxcox1p
+def plot_continuous(series):
+    fig= plt.figure(figsize=(12,8))
+    gs= GridSpec(2, 2) #row 2, col 2
+
+    ax= sns.scatterplot(y=train_detail.Weekly_Sales.values,x=boxcox1p(train_detail[series],.15),ax=fig.add_subplot(gs[0,1]),palette="blue")
+    plt.title("BoxCos .15\n"+"Corr: "+str(round(train_detail.Weekly_Sales.corr(boxcos1p(train_detail[series],.15)),2))+", Skew: "+str(round(stats.skew(boxcox1p(train_detail[series],.15),nan_policy="omit"),2)))
+
+    ax= sns.scatterplot(y=train_detail.Weekly_Sales,x=boxcox1p(train_detail[series],.25),ax=fig.add_subplot(gs[1,0]),palette="blue")
+    plt.title("BoxCos .25\n"+"Corr: "+str(round(train_detail.Weekly_Sales.corr(boxcos1p(train_detail[series],.25)),2))+", Skew: "+str(round(stats.skew(boxcox1p(train_detail[series],.25),nan_policy="omit"),2)))
+
+    ax= sns.displot(train_detail[series],ax=fig.add_subplot(gs[1,1]),color="green")
+    plt.title("Distribution\n")
+
+    ax= sns.scatterplot(y=train_detail.Weekly_Sales,x=train_detail[series],ax=fig.add_subplot(gs[0,0]),color="red")
+    plt.title("Linear\n"+"Corr: "+str(round(train_detail.Weekly_Sales.corr(train_detail[series]),2))+", Skew: "+str(round(stats.skew(train_detail[series],.25,nan_policy="omit"),2)))
+
+    # fig.show()
+    plt.show()
+    # End of function
+
+### Feature correlation
+### - weekly_sales X isHoliday
+plot_discrete("IsHoliday")
